ee689b47f25d67fec67e439631f0fbf6c5849f79
KACI OUSSAMA <73916588+kacioussama0@users.noreply.github.com>
2022-08-07 21:28:03 -0700

    Add files via upload

---
+++ components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="card shadow border-1 border-dark rounded">
+
+    <div class="card-header bg-white border-0 py-3 text-center">
+      <slot name="header" />
+    </div>
+
+    <div class="card-body">
+
+      <slot name="body"/>
+
+    </div>
+
+    <div class="card-footer bg-white border-0 ">
+      <slot name="footer"/>
+    </div>
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+
+
+
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <footer class="bg-black text-light py-3 mt-5">
+    <p class="mb-0 text-center">All Right Reserved 2022 &copy; By Web Store</p>
+  </footer>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
